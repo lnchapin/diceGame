1f7fa7ca763e946589876f1a084ce8fd970c2bf2
Lindsay Chapin <lnchapin@gmail.com>
2019-06-22 11:06:33 -0600

    dice

---
+++ src/Components/Die.js
@@ -0,0 +1,16 @@
+import React from 'react';
+import styled from 'styled-components';
+
+const Icon = styled.i`
+  font-size: 10rem;
+`
+
+const Die = (props) => {
+  console.log(props);
+  return(
+      <Icon className={`fas fa-dice-${props.number}`}></Icon>
+  )
+}
+
+
+export default Die
---
+++ src/Components/RollDice.js
@@ -0,0 +1,39 @@
+import React, { Component } from 'react';
+import Die from './Die';
+import { Row, Col } from 'reactstrap'
+
+class RollDice extends Component {
+  state={
+    die1: "one",
+    die2: "two",
+    shake: false,
+    clickable: true
+  }
+
+  rollMe = (e) => {
+    this.setState({clickable: false})
+    let dieSides = ['one', 'two', 'three', 'four', 'five', 'six']
+    let newOne = dieSides[Math.floor(Math.random()*(dieSides.length))]
+    let newTwo = dieSides[Math.floor(Math.random()*(dieSides.length))]
+    this.setState({die1: newOne, die2: newTwo});
+
+  }
+
+  render(){
+    return(
+      <>
+        <Row>
+          <Col sm={12} md={6}>
+            <Die number={this.state.die1} />
+          </Col>
+          <Col sm={12} md={6}>
+            <Die number={this.state.die2} />
+          </Col>
+        </Row>
+          {this.state.clickable ? <button onClick={this.rollMe}>Roll</button> : <button disabled>Rolling</button>}
+      </>
+    )
+  }
+}
+
+export default RollDice;
